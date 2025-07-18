c54aabcd68e8c19d9533414b87314eadbcd00b98
Si Thu Yan Naing <sithuyanniang@yomabank.com>
2025-07-18 16:16:09 +0630

    add try catch

--- src/databases/db.ts
+++ src/databases/db.ts
@@ -2,9 +2,13 @@ import mysql from "mysql2/promise";
 import config from "./config";
 
 const query = async (sql: string, params?: any) => {
-  const connection = await mysql.createConnection(config.db);
-  const [result] = await connection.query(sql, params);
-  return result;
+  try {
+    const connection = await mysql.createConnection(config.db);
+    const [result] = await connection.query(sql, params);
+    return result;
+  } catch (err) {
+    console.log("Connection Failed", err);
+  }
 };
 
 export default {
